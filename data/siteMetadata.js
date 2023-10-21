2cc630c4b539a35ebc7361d8b223c4dafa2d44ad
Amir Lavasani <amirm.lavasani@gmail.com>
2023-10-21 09:42:19 +0330

    Update site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Amir Lavaasni Portfolio and Blog',
+  author: 'Amir Lavasani',
   headerTitle: 'Amir Lavasani',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'My personal blog and portfolio to share my writings and projects.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://amirlavasani.vercel.app/',
+  siteRepo: 'https://github.com/AmirLavasani/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'amirm.lavasani@gmail.com',
+  github: 'https://github.com/AmirLavasani',
+  twitter: 'https://twitter.com/AmirMLavasani',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/amir-lavasani/ ',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
